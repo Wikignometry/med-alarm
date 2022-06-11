8747597fa0af71ca6fb8f962536cd7a02161b71e
Wikignometry <pphanach@andrew.cmu.edu>
2022-06-11 18:52:37 -0400

    added timePicker

--- app/src/main/java/com/medalarm/medalarm/MainActivity.kt
+++ app/src/main/java/com/medalarm/medalarm/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.medalarm.medalarm
 
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import com.medalarm.medalarm.ui.theme.MedAlarmTheme
+import java.util.*
 
 
 
@@ -22,26 +29,59 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MedAlarmTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Column{
-                        ToastButton("I just took my pill", "Time logged!")
-                    }
-
-                }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            )
+            {
+                ToastButton("I just took my pill", "Time logged!")
+                TimePickerButton()
+                SimpleOutlinedTextFieldSample()
             }
         }
     }
 }
 
+// from https://github.com/Kiran-Bahalaskar/Time-Picker-With-Jetpack-Compose/blob/master/app/src/main/java/com/kiranbahalaskar/timepicker/MainActivity.kt
+@Composable
+fun TimePickerButton() {
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+    val hour = calendar[Calendar.HOUR_OF_DAY]
+    val minute = calendar[Calendar.MINUTE]
+    val time = remember { mutableStateOf("") }
 
+    val timePickerDialog = TimePickerDialog(
+        context,
+        { _, hour: Int, minute: Int ->
+            time.value = "$hour:$minute"
+        }, hour, minute, false
+    )
+        Button(onClick = {
+            timePickerDialog.show()
+        }) {
+            Text(text = "I already took my pill")
+        }
+    Text(text = "I took my first pill at: ${time.value}")
+}
+
+// from https://developer.android.com/jetpack/compose/text
+@Composable
+fun SimpleOutlinedTextFieldSample() {
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Label") }
+    )
+}
 
 @Composable
 fun ToastButton(label: String, text: String) {
     val context = LocalContext.current
     Button(onClick = {
-        val duration = Toast.LENGTH_LONG
+        val duration = Toast.LENGTH_SHORT
         val toast = Toast.makeText(context, text, duration)
         toast.show()
     }) {
