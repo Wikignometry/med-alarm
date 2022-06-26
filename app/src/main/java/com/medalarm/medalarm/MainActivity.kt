2beabc416ef82e06842cf01198b4565f7a448ddf
Benjamin O <jeep70cp@gmail.com>
2022-06-26 14:43:37 -0700

    Redesign UI and go in too deep on experimental UI libraries

--- app/src/main/java/com/medalarm/medalarm/MainActivity.kt
+++ app/src/main/java/com/medalarm/medalarm/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.medalarm.medalarm
 
-import android.app.TimePickerDialog
+import android.content.Context
+import android.content.ContextWrapper
 import android.os.Bundle
 import android.text.format.DateFormat
-import androidx.activity.ComponentActivity
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAlarm
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,70 +21,141 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.fragment.app.FragmentActivity
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import com.medalarm.medalarm.ui.theme.MedAlarmTheme
+import com.medalarm.medalarm.ui.theme.isLight
 import java.sql.Time
 import java.text.SimpleDateFormat
 import java.util.*
 
-
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxWidth()
-            )
-            {
+            MedAlarmTheme {
+                val systemUiController = rememberSystemUiController()
+                systemUiController.setSystemBarsColor(
+                    color = MaterialTheme.colorScheme.background,
+                    darkIcons = MaterialTheme.colorScheme.isLight()
+                )
+
                 TimerScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun TimerScreen() {
     val context = LocalContext.current
-    var sdf = SimpleDateFormat("h::mm aa")
+    var sdf = SimpleDateFormat("h:mm aa")
     if (DateFormat.is24HourFormat(context)) {
-        sdf = SimpleDateFormat("H::mm")
+        sdf = SimpleDateFormat("H:mm")
     }
     val currMs = System.currentTimeMillis()
-    var time: Time by rememberSaveable { mutableStateOf(Time(currMs)) }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(32.dp)
-    ){
-        TimePickerButton ({ ms ->
-            time = Time(ms)
-        }) {
-            Text("I just took my pill")
-        }
+    var time by rememberSaveable { mutableStateOf(Time(currMs)) }
+    var count = rememberSaveable { mutableStateOf(3) } // TODO: Make this save across app restarts
+    var endTime by rememberSaveable { mutableStateOf(Time(currMs)) } // TODO: Make this default to something reasonable and persist across app restarts
 
-        TimePickerButton ({ ms ->
-            time = Time(ms)
-        }) {
-            Text("My last pill will be taken at")
-        }
+    val textSize = 20.sp
 
-        Text("I first took my pill at ${sdf.format(time)}")
-//        ShowTimePicker("I already took my pill")
-//        Text("next one")
-//        ShowTimePicker("Last pill taken at")
+    Scaffold(
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { /*TODO*/ },
+                icon = { Icon(Icons.Default.AddAlarm, "Add alarms") },
+                text = { Text(text = "Create Alarms") }, // TODO: make s conditional on alarm count
+            )
+        },
+    ) { paddingValues ->
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            contentPadding = paddingValues,
+            modifier = Modifier.padding(32.dp)
+        ) {
+            item {
+                Text(buildAnnotatedString {
+                    append("I first took my pill at ")
 
-        Divider(modifier = Modifier.padding(16.dp))
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("${sdf.format(time)}")
+                    }
+                }, fontSize = textSize)
+            }
 
-        Text("I will take my pill 3 more times today", fontSize = 24.sp)
+            item {
+                TimePickerButton({ ms ->
+                    time = Time(ms)
+                }) {
+                    Icon(Icons.Default.Edit, "Edit")
+                    Text("Edit")
+                }
+            }
 
-        Divider(modifier = Modifier.padding(16.dp))
+            item {
+                Divider(modifier = Modifier.padding(16.dp))
+            }
 
-        Text("My last pill will be taken at ", fontSize = 24.sp)
+            // part 2
 
-        Button({}) {
-            Text("1:30 PM")
+            item {
+                Text(buildAnnotatedString {
+                    append("I will take my pill ")
+
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("${count.value}")
+                    }
+
+                    append(" more times today")
+                }, fontSize = textSize)
+            }
+
+            item {
+                Button(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Default.Edit, "Edit")
+                    Text("Edit")
+                }
+            }
+
+            item {
+                Divider(modifier = Modifier.padding(16.dp))
+            }
+
+            // part 3
+
+            item {
+                Text(buildAnnotatedString {
+                    append("My last pill will be taken at ")
+
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("${sdf.format(endTime)}")
+                    }
+                }, fontSize = textSize)
+            }
+
+            item {
+                TimePickerButton({ ms ->
+                    endTime = Time(ms)
+                }) {
+                    Icon(Icons.Default.Edit, "Edit")
+                    Text("Edit")
+                }
+            }
         }
     }
 }
@@ -90,28 +166,55 @@ fun TimerScreen() {
 fun TimePickerButton(onValueChange: (Long) -> Unit,
                      content: @Composable() () -> Unit) {
     val context = LocalContext.current
-    val calendar = Calendar.getInstance()
-    val hour = calendar[Calendar.HOUR_OF_DAY]
-    val minute = calendar[Calendar.MINUTE]
-    val timePickerDialog = TimePickerDialog(
-        context,
-        {
-            _, hr, min ->
-            val convCal = Calendar.getInstance()
-            convCal.set(Calendar.HOUR_OF_DAY, hr)
-            convCal.set(Calendar.MINUTE, min)
-            convCal.set(Calendar.SECOND, 0)
-            convCal.set(Calendar.MILLISECOND, 0)
-
-            onValueChange(convCal.timeInMillis)
-        },
-        hour, minute, DateFormat.is24HourFormat(context)
-    )
     Button(onClick = {
-        timePickerDialog.show()
+        showTimePicker(context, onValueChange)
     }) {
         content()
     }
+}
+
+// Extension functions are wonderful! https://stackoverflow.com/a/68423182
+tailrec fun Context.getActivity(): FragmentActivity? = when (this) {
+    is FragmentActivity -> this
+    is ContextWrapper -> baseContext.getActivity()
+    else -> null
+}
+
+fun showTimePicker(context: Context, onValueChange: (Long) -> Unit) {
+    val tag = "medalarm"
+
+    val activity = context.getActivity()
+    if (activity == null) {
+        Log.e(tag, "Activity was null!")
+        return
+    }
+
+
+    val calendar = Calendar.getInstance()
+    val hour = calendar[Calendar.HOUR_OF_DAY]
+    val minute = calendar[Calendar.MINUTE]
+
+
+    val picker =
+        MaterialTimePicker.Builder()
+            .setTimeFormat(if (DateFormat.is24HourFormat(context)) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H)
+            .setHour(hour)
+            .setMinute(minute)
+            .build()
+
+    picker.addOnPositiveButtonClickListener { _ ->
+        val hr = picker.hour
+        val min = picker.minute
+        val convCal = Calendar.getInstance()
+        convCal.set(Calendar.HOUR_OF_DAY, hr)
+        convCal.set(Calendar.MINUTE, min)
+        convCal.set(Calendar.SECOND, 0)
+        convCal.set(Calendar.MILLISECOND, 0)
+
+        onValueChange(convCal.timeInMillis)
+    }
+
+    picker.show(activity.supportFragmentManager, tag)
 }
 
 // from https://developer.android.com/jetpack/compose/text
--- app/src/main/java/com/medalarm/medalarm/ui/theme/Theme.kt
+++ app/src/main/java/com/medalarm/medalarm/ui/theme/Theme.kt
@@ -1,44 +1,43 @@
 package com.medalarm.medalarm.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
+@Composable
+fun ColorScheme.isLight() = this.background.luminance() > 0.5
+
+// Converted UI theme from previous default (should be changed to red or something nice :)
+private val LightColorScheme = lightColorScheme(
+    primary = Purple200,
+    secondary = Purple700,
+    tertiary = Teal200,
+    // error, primaryContainer, onSecondary, etc.
+)
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple500,
+    secondary = Purple700,
+    tertiary = Teal200,
+    // error, primaryContainer, onSecondary, etc.
 )
 
-private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
-
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
 
 @Composable
 fun MedAlarmTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
 
     MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
